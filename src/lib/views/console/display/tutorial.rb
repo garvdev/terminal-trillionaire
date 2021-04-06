175903e451b3e32ee41ae4c942686d989c9fa7f9
garvdev <garvey.chan@icloud.com>
2021-04-06 23:08:19 +1000

    Add tutorial story text

--- src/lib/views/console/display/tutorial.rb
+++ src/lib/views/console/display/tutorial.rb
@@ -15,25 +15,25 @@ module Views
                     win.clear 
                     sleep 1 
 
-                    # tutorial = [["Hey there, #{user}!        ",:all,2],
-                    #         ["It's good to see you again.",:old,2],
-                    #         ["It's good to have you here.",:new,2],
-                    #         ["Welcome to.                ",:new,1],
-                    #         ["Welcome to..               ",:new,1],
-                    #         ["Welcome to...              ",:new,1],
-                    #         ["Welcome back to.           ",:old,1],
-                    #         ["Welcome back to..          ",:old,1],
-                    #         ["Welcome back to...         ",:old,1],
-                    #         ["                           ",:all,0]]
-                    # tutorial.each do |msg|
-                    #     next if msg[1] != :all && msg[1] != status
-                    #     win.setpos(1,2)
-                    #     win.addstr("#{msg[0]}")
-                    #     win.refresh
-                    #     sleep msg[2]
-                    # end
+                    tutorial = [["The year is 2050...\n",:all,3],
+                                ["High speed automated trading bots have squeezed all the alpha from the market;\n",:all,3],
+                                ["Poor housing market policy has left all the wealth concentrated in the top 0.1% of society;\n",:all,3],
+                                ["Elon Musk is still manipulating securities and cryptocurrency on Twitter.\n",:all,4],
+                                ["But there is hope...\n",:all,3],
+                                ["You are a precocious 6 year old who has stumbled upon an old terminal in the attic.\n",:all,3],
+                                ["Intrigued, you pick it up and play with the controls, feeling a slight tingle in your fingertips.\n",:all,3],
+                                ["'Must be the static', you think to yourself as you try to find Pacman.rb in the games section.\n",:all,3],
+                                ["After a fruitless search, you give up and decide to settle with 'Terminal Trillionaire'.\n",:all,3],
+                                ["'I guess this could kill an afternoon or two', you say out loud, speaking to nobody in particular.\n",:all,3],
+                                ["Little do you know, the future of the world's economic health and prosperity rests on your shoulders...\n",:all,3]]
 
-                    win.addstr("\n  Press any key to continue to the console.")
+                    tutorial.each do |msg|
+                        win.addstr("#{msg[0]}")
+                        win.refresh
+                        sleep msg[2]
+                    end
+
+                    win.addstr("\nPress any key to continue to the console.")
                     win.refresh
                     
                     win.getch
