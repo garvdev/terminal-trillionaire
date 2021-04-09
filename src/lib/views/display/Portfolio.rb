390b668569b15cae093fd4100ad03acb5dd41039
garvdev <garvey.chan@icloud.com>
2021-04-09 14:31:20 +1000

    Add totals, P&L and summary to portfolio view

--- src/lib/views/display/Portfolio.rb
+++ src/lib/views/display/Portfolio.rb
@@ -31,6 +31,7 @@ module Views
 
                         current_prices = {}
                         table = []
+                        summary = [[]]
                         
                         user.file[:holdings].each_pair do |k,v|
                             table << [k,v[0],v[1],v[1]/v[0]]
@@ -42,18 +43,27 @@ module Views
                         
                         table.each do |x| 
                             x << current_prices.fetch(x[0]).to_f
-                            
-                            # x << (x.last * x[1])
-                            # x << (x.last - x[2])
-                            # x.map! {|x| x.is_a?(Symbol) ? x : number_comma(x)}
+                            x << (x.last * x[1])
+                            x << (x.last - x[2])
+                        end
+                        
+                        summary[0][0] = number_comma(table.map{|x| x[5]}.sum)
+                        summary[0][1] = number_comma(table.map{|x| x[6]}.sum)
+                        
+                        table.each do |x|
+                            x.map! {|y| y.is_a?(Symbol) ? y : number_comma(y)}
                         end
 
-                        win.addstr("#{table}#{current_prices}") #testing
-                        win.refresh #testing
+                        # win.addstr("#{totals}#{table}#{current_prices}") #testing
+                        # win.refresh #testing
                         
-                        # tty_table = TTY::Table.new(header: [" Security ", "Quantity ", "Total Cost Basis", "Avg Cost Basis", "Current Price", "Total Value", "P&L"], rows: table)
-                        # win.addstr("#{tty_table.render(:unicode, alignments: [:center,:right,:right,:right,:right,:right,:right,:right])}") 
-                        # win.refresh
+                        table_main = TTY::Table.new(header: [" Security ", " Quantity ", " Total Cost Basis ", " Avg Cost Basis ", " Current Price ", " Total Value ", " Profit/Loss "], rows: table)
+                        table_summary = TTY::Table.new(header: [" Portfolio Value "," Portfolio P&L "], rows: summary)
+
+                        win.addstr("#{table_main.render(:unicode, alignments: [:center,:right,:right,:right,:right,:right,:right,:right])}") 
+                        win.addstr("\n#{table_summary.render(:unicode, alignments: [:right,:right])}") 
+
+                        win.refresh
                     end
                 end
 
