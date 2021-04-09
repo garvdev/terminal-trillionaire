e61e412814d7f2804bb526ae11151bb3d3313eaf
garvdev <garvey.chan@icloud.com>
2021-04-09 13:10:21 +1000

    Add current price floats to portfolio table

--- src/lib/models/market/Catalogue.rb
+++ src/lib/models/market/Catalogue.rb
@@ -3,14 +3,15 @@ require_relative "./SecurityItem.rb"
 
 module Market
     module Catalogue
-        TICKERS = [:PEAR,:EDSN,:CHLL,:YMMY,:EXCL,:WATR,:LAMP,:TEEM,:SOLA,:CODE,
+        TICKERS = [:CASH,:PEAR,:EDSN,:CHLL,:YMMY,:EXCL,:WATR,:LAMP,:TEEM,:SOLA,:CODE,
         :TEXT,:FODR,:MUSC,:BIKE,:SCNT,:MAKE,:DSGN,:ANML,:LOOP,:SLAM]
 
         def self.price_list
             price_list = {}
             TICKERS.each do |ticker|
+                (price_list[:CASH] = {current: 1, day: 1, month: 1, year: 1, decade: 1}; next) if ticker == :CASH
                 t = SecurityItem.new(ticker.to_s)       
-                price_list[t.ticker] = t.prices
+                price_list[t.ticker.to_sym] = t.prices
             end
             price_list
         end
--- src/lib/views/display/Portfolio.rb
+++ src/lib/views/display/Portfolio.rb
@@ -33,16 +33,27 @@ module Views
                         table = []
                         
                         user.file[:holdings].each_pair do |k,v|
-                            table << [k,v[0],v[1],v[1]/v[0]].map {|x| x.is_a?(Symbol) ? x : number_comma(x)}
+                            table << [k,v[0],v[1],v[1]/v[0]]
                         end
 
                         yield.each_pair do |k,v|
-                            current_prices[k] = "#{number_comma(v[:current])}"
+                            current_prices[k] = "#{v[:current]}"
+                        end
+                        
+                        table.each do |x| 
+                            x << current_prices.fetch(x[0]).to_f
+                            
+                            # x << (x.last * x[1])
+                            # x << (x.last - x[2])
+                            # x.map! {|x| x.is_a?(Symbol) ? x : number_comma(x)}
                         end
 
-                        tty_table = TTY::Table.new(header: [" Security ", "Quantity ", "Total Cost Basis", "Avg Cost Basis", "Current Price", "Total Value", "P&L"], rows: table)
-                        win.addstr("#{tty_table.render(:unicode, alignments: [:center,:right,:right,:right,:right,:right,:right,:right])}") 
-                        win.refresh
+                        win.addstr("#{table}#{current_prices}") #testing
+                        win.refresh #testing
+                        
+                        # tty_table = TTY::Table.new(header: [" Security ", "Quantity ", "Total Cost Basis", "Avg Cost Basis", "Current Price", "Total Value", "P&L"], rows: table)
+                        # win.addstr("#{tty_table.render(:unicode, alignments: [:center,:right,:right,:right,:right,:right,:right,:right])}") 
+                        # win.refresh
                     end
                 end
 
