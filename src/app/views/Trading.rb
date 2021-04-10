80183aca67d6813bf9015aa533f4eaff8d8b3e70
garvdev <garvey.chan@icloud.com>
2021-04-10 15:51:03 +1000

    Improve buying logic and add guards for negative/insufficient cash

--- src/app/views/Trading.rb
+++ src/app/views/Trading.rb
@@ -29,12 +29,24 @@ module Views
                 if trade_type == true
                     trade_ticker = tty_prompt.select("\nTime to put your money where your mouth is!\nWhich security would you like to transact with today?\n", Catalogue::TICKERS[1..Catalogue::TICKERS.length], cycle: true, show_help: :always)
                     locked_price = SecurityPricing.prices(trade_ticker)[:current]
-                    puts "\nGreat choice! Our brokers have locked in a price of #{("$"+number_comma(locked_price)).light_green} for you."
+
+                    
+                    puts "\nGreat choice! Our brokers have locked in a unit price of #{("$"+number_comma(locked_price)).light_green} for you."
+                    (puts "\nHowever, it appears that you have insufficient cash to fund this purchase.\nConsider liquidating some of your existing holdings if you wish to make further purchases."; sleep_keypress(10,STDIN); next) if cash < locked_price
                     puts "With your current cash balance, you can purchase a maximum of #{(number_comma((cash/locked_price).floor.to_i)).light_cyan} shares of #{trade_ticker.to_s.light_blue}."
                     puts "How much #{trade_ticker.to_s.light_blue} would you like to order today?"
-                    trade_qty = Input.get("int")
-                    confirmed = tty_prompt.yes?("\nConfirming you would like to purchase #{number_comma(trade_qty).light_cyan} shares of #{trade_ticker.to_s.light_blue}, at a unit price of #{("$"+number_comma(locked_price)).light_green} for a total of #{("$"+number_comma(locked_price*trade_qty)).light_green}?") {|q| q.suffix "y/n"}
-                    confirmed ? (puts; tty_cursor.invisible{20.times {sleep 0.1;tty_bar.advance}}; puts "Trade executed successfully!"; yield [trade_ticker, trade_qty, locked_price] ; yield [:CASH, -(trade_qty*locked_price), 1]) : next
+                    
+                    i = 0
+                    while true
+                        trade_qty = Input.get("int")
+                        break if trade_qty <= cash/locked_price
+                        i == 0 ? (puts "You don't have enough cash for that! Please try a different number.") : (print tty_cursor.clear_lines(2))
+                        i += 1
+                    end
+                    
+                    puts; confirmed = tty_prompt.yes?("Confirming you would like to purchase #{number_comma(trade_qty).light_cyan} shares of #{trade_ticker.to_s.light_blue}, at a unit price of #{("$"+number_comma(locked_price)).light_green} for a total of #{("$"+number_comma(locked_price*trade_qty)).light_green}?") {|q| q.suffix "y/n"}
+                    puts; confirmed ? (tty_cursor.invisible{20.times {sleep 0.1;tty_bar.advance}}; puts "Trade executed successfully!"; yield [trade_ticker, trade_qty, locked_price] ; yield [:CASH, -(trade_qty*locked_price), 1]) : next
+                    puts; tty_prompt.yes?("Would you like to execute further trades?") {|q| q.suffix "y/n"} ? next : break
                 else
                     # puts ""
                     # "Never hang on to a loser!"
@@ -43,7 +55,6 @@ module Views
 
 
                 # p trade_type; p trade_ticker
-                return [:CASH,10000.12,1] # test trade
                 # insufficient cash/stock logic
                 # LAST IN FIRST OUT - sell behaviour - traverse trade history
             end
